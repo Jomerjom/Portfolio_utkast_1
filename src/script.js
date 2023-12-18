e90eec4f20a4c2cd8964526e3f2566af4c3f3a0d
Jomerjom <ekenesjoachim@gmail.com>
2023-12-18 12:03:13 +0100

    added placeholder image and some basic information

---
+++ src/script.js
@@ -0,0 +1,104 @@
+const projects = [
+    {
+        name: 'Project 1',
+        description: 'This is a project',
+        image: 'https://via.placeholder.com/300',
+        link: 'https://www.google.com',
+        github: 'https://github.com',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        name: 'Project 2',
+        description: 'This is a project',
+        image: 'https://via.placeholder.com/300',
+        link: 'https://www.google.com',
+        github: 'https://github.com',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        name: 'Project 3',
+        description: 'This is a project',
+        image: 'https://via.placeholder.com/300',
+        link: 'https://www.google.com',
+        github: 'https://github.com',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        name: 'Project 4',
+        description: 'This is a project',
+        image: 'https://via.placeholder.com/300',
+        link: 'https://www.google.com',
+        github: 'https://github.com',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+];
+
+const technologies = [
+    {
+        name: 'HTML5',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'CSS3',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Javascript',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'React',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'SASS',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Tailwind',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Node.js',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Java',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Python',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'MySQL',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Git',
+        image: 'https://via.placeholder.com/50',
+    },
+    {
+        name: 'Github',
+        image: 'https://via.placeholder.com/50',
+    }
+];
+
+const socials = [
+    {
+        name: 'LinkedIn',
+        image: 'https://via.placeholder.com/50',
+        link: 'https://www.linkedin.com/in/brandon-lee-8a2b3b1b4/'
+    },
+    {
+        name: 'Github',
+        image: 'https://via.placeholder.com/50',
+        link: 'https://github.com/Jomerjom'
+    },
+    {
+        name: 'Email',
+        image: 'https://via.placeholder.com/50',
+        link: 'ekenesjoachim@gmail.com'
+    }
+];
+
